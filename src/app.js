863f084dd96b2cebb24f54b46fa1e062ae050439
mossa-Sammer <mossa708496@gmail.com>
2019-09-04 21:27:53 +0300

    handle statics and add some style

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-const express = require('express');
+// core modules
+const { join } = require('path');
 
+// 3'rd party modules
+const express = require('express');
 const cookieParser = require('cookie-parser');
 
+// local modules
 const router = require('./controllers');
 
 
@@ -13,5 +17,8 @@ app.set('port', port);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use(router);
+
 module.exports = app;
