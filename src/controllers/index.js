740fd8134b3cbfccae536317b34e4a52c6e17ee0
mossa-Sammer <mossa708496@gmail.com>
2019-09-05 11:31:52 +0300

    haandle server and client errors

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const postUser = require('../signup/index');
+const { clientError, serverError } = require('./error');
 
 router.get('/', (req, res) => {
   res.send('keep it simple for now');
 });
 
 router.use(postUser);
+router.use(clientError);
+router.use(serverError);
 module.exports = router;
